bf4f40dcb1b51b0f3e6133619e52f4b2d9f7226f
Daniel Narey <contact@danielnarey.com>
2020-12-26 21:06:56 -0500

    initial

---
+++ test/index.js
@@ -0,0 +1,37 @@
+import { resolve } from 'path';
+import { existsSync, rmSync } from 'fs';
+
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+
+import { copy } from '../lib/index.js';
+
+
+const context = resolve(process.cwd(), 'test/fixtures');
+const dist = 'public';
+
+const expected = [
+  '1a.txt',
+  '1b.txt',
+  '2a.txt',
+  '2b.txt',
+];
+
+
+test.before(async () => {
+  await copy(context, { dist });
+});
+
+
+test.after(() => {
+  rmSync(resolve(context, dist), { force: true });
+});
+
+
+test('All files were copied', () => {
+  assert.ok(
+    expected.every(
+      (name) => existsSync(path.resolve(context, dist, 'files', name)),
+    ),
+  );
+});
